7d6c2c54ff1cfee051eb8b3b04ea494b64cecad8
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-17 21:57:19 +0530

    validating user through if else

---
+++ validate_users_data/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var email string
+	var age int
+
+	fmt.Println("Enter your name :")
+	fmt.Scan(&name)
+	fmt.Println("Enter your email :")
+	fmt.Scan(&email)
+	fmt.Println("Enter your age :")
+	fmt.Scan(&age)
+
+	if name == "" || len(name) < 2 || email == "" || age == 0 || age > 18 {
+		fmt.Println("Please correctly  filled  all the fields")
+	} else {
+		fmt.Println("Name : ", name)
+		fmt.Println("Email : ", email)
+		fmt.Println("Age : ", age)
+	}
+
+}
