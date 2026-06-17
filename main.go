745f40bf3319d95398deb4d77733e8251fd8dfd7
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-17 19:32:49 +0530

    Taking input

--- main.go
+++ main.go
@@ -8,9 +8,19 @@ func main() {
 
 	var confreneceName = "Go Conference"
 	const confreneceTickets = 50
+	var remainingTickets = confreneceTickets
 
-	fmt.Println("welcome to",confreneceName,"booking application")
-	fmt.Println("Number of tickets available :",confreneceTickets)
+	fmt.Println("welcome to", confreneceName, "booking application")
+	fmt.Println("Number of tickets available :", remainingTickets)
+
 	fmt.Println("Get your tickets now")
+
+	var userName string
+	// asking user for their name
+
+	fmt.Println("Enter Your name")
+	fmt.Scan(&userName)
+
+	println("Hello", userName)
 
 }
