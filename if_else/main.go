d074d28f99f12496084efab7c4d4a0c187e7f21e
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-17 21:44:18 +0530

    more code added

--- if_else/main.go
+++ if_else/main.go
@@ -14,4 +14,31 @@ func main() {
 		fmt.Println("you can't vote")
 	}
 
+	// if-else if ladder
+	score := 85
+	if score >= 90 {
+		fmt.Println("Grade: A")
+	} else if score >= 80 {
+		fmt.Println("Grade: B")
+	} else if score >= 70 {
+		fmt.Println("Grade: C")
+	} else {
+		fmt.Println("Grade: F")
+	}
+
+	// if with short statement
+	if num := 15; num%2 == 0 {
+		fmt.Println(num, "is even")
+	} else {
+		fmt.Println(num, "is odd")
+	}
+
+	// nested if
+	x := 10
+	y := 20
+	if x == 10 {
+		if y == 20 {
+			fmt.Println("x=10 and y=20")
+		}
+	}
 }
