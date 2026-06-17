01d6993d39c1489d54ea416d69dc77712769a01a
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-17 19:21:06 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World")
+
+	var confreneceName = "Go Conference"
+	const confreneceTickets = 50
+
+	fmt.Println("welcome to",confreneceName,"booking application")
+	fmt.Println("Number of tickets available :",confreneceTickets)
+	fmt.Println("Get your tickets now")
+
+}
